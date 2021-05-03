063c63874230f6714957aed4e994833d358c281a
Oldrich Panacek <panacek.olin@seznam.cz>
2021-05-03 10:29:06 +0200

    fibonachi recurse implemented

--- fibo.py
+++ fibo.py
@@ -1,6 +1,20 @@
+def fibo(n):
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n - 1) + fibo(n - 2)
+
 
 def main():
-    pass
+    number = int(input("Zadejte číslo:"))
+    while number < 1:
+        print("Zadali jste neplatné číslo.")
+        number = int(input("Zadejte číslo:"))
+    else:
+        return print(fibo(number - 1))
 
 
 if __name__ == '__main__':
